d1dab901cade897cef5f6cbe27e753a96f408b9c
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-08-13 10:34:57 -0700

    Initial Commit

---
+++ SeanAllenAPI/SeanAllenAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeanAllenAPIApp.swift
+//  SeanAllenAPI
+//
+//  Created by Tatsuya Moriguchi on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SeanAllenAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
